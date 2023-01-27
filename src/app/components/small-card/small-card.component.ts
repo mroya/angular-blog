b72ada6addd61c32e3f710edbc89eba445dbad8a
mroya <royamarcio@gmail.com>
2023-01-27 13:31:28 -0300

    created responsive components

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,14 +3,17 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: [
+    './small-card.component.css',
+    './small-card.responsive.component.css'
+  ]
 })
 export class SmallCardComponent implements OnInit {
 
   @Input()
-  photoCover:string = ""
+  photoCover:string = "";
   @Input()
-  cardTitle:string = ""
+  cardTitle:string = "";
 
   constructor() {}
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './home.component.css',
+    './home.responsive.component.css'
+  ]
 })
 export class HomeComponent {
 
